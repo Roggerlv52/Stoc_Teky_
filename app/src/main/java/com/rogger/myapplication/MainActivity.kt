0dded1b110db081c6fa642aa310cd4ec24da0ffc
Roggerlvr52 <roggerlvr52@gmail.com>
2025-03-23 01:26:40 +0000

    Adicionado mais detalhes em algumas linhas de codigo.

--- app/src/main/java/com/rogger/myapplication/MainActivity.kt
+++ app/src/main/java/com/rogger/myapplication/MainActivity.kt
@@ -1,53 +1,160 @@
 package com.rogger.myapplication
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private var doubleBackToExitPressedOnce = false
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // installSplashScreen()
+        // installSplashScreen()
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-         //toolbar.setBackgroundColor(android.graphics.Color.TRANSPARENT)
-        toolbar.setTitleTextColor(getResources().getColor(R.color.white))
+        //toolbar.setBackgroundColor(android.graphics.Color.TRANSPARENT)
+        toolbar.setTitleTextColor(Color.WHITE)
         setSupportActionBar(toolbar)
         // Define um ícone personalizado
-        //toolbar.setNavigationIcon(R.drawable.ic_hamburg_menu_24)
 
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+         drawerLayout = findViewById(R.id.drawer_layout)
+
         val navView = findViewById<NavigationView>(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+         navController = findNavController(R.id.nav_host_fragment_content_main)
 
+        val btnFinish = findViewById<Button>(R.id.drawer_btn_finish)
+        var cont = 0
+        btnFinish.setOnClickListener {
+            cont++
+            Toast.makeText(this, "Clique mais uma vez para sair ", Toast.LENGTH_SHORT).show()
+            if (cont == 2) {
+                Toast.makeText(this, "Sessão Encerrada!", Toast.LENGTH_SHORT).show()
+                finishAffinity()
+            }
+        }
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.nav_home, R.id.nav_gallery,R.id.nav_setting),drawerLayout
+            setOf(R.id.nav_home, R.id.nav_helper, R.id.nav_setting, R.id.nav_btn_help),
+            drawerLayout
         )
-        setupActionBarWithNavController(navController,appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        // Configura o callback para o botão de voltar
+        screenFinish()
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_btn_sharad -> {
+                    // Ação para o item "Compartilhar"
+                    //shareAppViaWhatsApp()
+                    shareAppViaOther()
+                    drawerLayout.closeDrawers()
+                    true
+                }
 
-
+                R.id.nav_btn_avaliar -> {
+                    // Ação para o item "Avaliar"
+                    openGmail()
+                    drawerLayout.closeDrawers()
+                    true
+                }
+                R.id.nav_btn_site -> {
+                    // Ação para o item "Site"
+                    val url = "https://www.google.com"
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(intent)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+                else -> {
+                    // Deixa o NavController lidar com a navegação para outros itens
+                    // Isso é crucial para o AppBarConfiguration funcionar corretamente
+                    navController.navigate(menuItem.itemId)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return super.onCreateOptionsMenu(menu)
     }
-    
+
+    private fun screenFinish() {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (doubleBackToExitPressedOnce) {
+                    finishAffinity() // Encerra todas as atividades do aplicativo
+                    return
+                }
+
+                doubleBackToExitPressedOnce = true
+                Toast.makeText(
+                    this@MainActivity,
+                    "Clique mais uma vez para sair ",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    doubleBackToExitPressedOnce = false
+                }, 2000) // Tempo de espera de 2 segundos
+            }
+        })
+    }
+
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun shareAppViaOther() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "Confira este aplicativo incrível: [link para o app]")
+        startActivity(Intent.createChooser(intent, "Compartilhar via"))
+    }
+
+    private fun openGmail() {
+        val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:") // Somente aplicativos de e-mail devem lidar com isso
+            putExtra(
+                Intent.EXTRA_EMAIL,
+                arrayOf("suporte@stocteky.com")
+            ) // Substitua pelo endereço de e-mail do destinatário
+            putExtra(Intent.EXTRA_SUBJECT, "") // Substitua pelo assunto do e-mail
+            putExtra(Intent.EXTRA_TEXT, "") // Substitua pelo corpo do e-mail
+        }
+        // Verifica se existe algum aplicativo que pode lidar com a Intent
+        if (emailIntent.resolveActivity(packageManager) != null) {
+            startActivity(emailIntent)
+        } else {
+            // Nenhum aplicativo de e-mail encontrado
+            Log.e("MainActivity", "Nenhum aplicativo de e-mail encontrado")
+        }
     }
 }
--- app/src/main/java/com/rogger/myapplication/ui/helper/HelperViewModel.java
+++ app/src/main/java/com/rogger/myapplication/ui/helper/HelperViewModel.java
@@ -1,14 +1,14 @@
-package com.rogger.myapplication.ui.register.view;
+package com.rogger.myapplication.ui.helper;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class HelperViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public HelperViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
